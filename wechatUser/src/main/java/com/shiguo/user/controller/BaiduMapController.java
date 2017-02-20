8394026f0ee3f43ba429a64627695b56a3d00d98
shixj@inhand.com.cn <shixj@inhand.com.cn>
2017-02-20 16:26:49 +0800

    修改配送范围设置

--- wechatUser/src/main/java/com/shiguo/user/controller/BaiduMapController.java
+++ wechatUser/src/main/java/com/shiguo/user/controller/BaiduMapController.java
@@ -1,6 +1,7 @@
 package com.shiguo.user.controller;
 
 import com.shiguo.user.util.MapUtil;
+import java.awt.geom.Point2D;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,8 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
@@ -29,6 +32,28 @@ public class BaiduMapController {
 
     @Autowired
     RestTemplate restTemplate;
+    
+    @RequestMapping(value = "/IsPtInPoly", method = RequestMethod.GET)
+    public @ResponseBody
+    boolean IsPtInPoly(
+            @RequestParam(value = "lng", required = true) double lng,
+            @RequestParam(value = "lat", required = true) double lat) throws IOException {
+         
+         List<Point2D.Double> pts = new ArrayList<Point2D.Double>();
+         pts.add(new Point2D.Double(116.360338,40.105302));//龙禧医院
+         pts.add(new Point2D.Double(116.333172,40.096798));//北京昌平向上学校
+         pts.add(new Point2D.Double(116.318227,40.086762));//佰嘉城小区
+         pts.add(new Point2D.Double(116.354082,40.07282));//龙旗购物中心
+         pts.add(new Point2D.Double(116.362961,40.078128));//龙跃苑东五区
+         pts.add(new Point2D.Double(116.396583,40.091608));//回南家园
+         pts.add(new Point2D.Double(116.385082,40.104757));//平西王府总站
+         
+         Point2D.Double point = new Point2D.Double(lng, lat);
+         
+         boolean result  = IsPtInPoly(point, pts);
+         return result;
+    }
+    
     
     @RequestMapping(value = "/baidu/getLocation", method = RequestMethod.GET)
     public @ResponseBody
@@ -115,4 +140,87 @@ public class BaiduMapController {
         return sb.toString();
     }
     
+    public  void main(String[] args) throws IOException {
+    
+    Point2D.Double point = new Point2D.Double(116.404072, 39.916605);
+    
+    List<Point2D.Double> pts = new ArrayList<Point2D.Double>();
+    pts.add(new Point2D.Double(116.360338,40.105302));//龙禧医院
+    pts.add(new Point2D.Double(116.333172,40.096798));//北京昌平向上学校
+    pts.add(new Point2D.Double(116.318227,40.086762));//佰嘉城小区
+    pts.add(new Point2D.Double(116.354082,40.07282));//龙旗购物中心
+    pts.add(new Point2D.Double(116.362961,40.078128));//龙跃苑东五区
+    pts.add(new Point2D.Double(116.396583,40.091608));//回南家园
+    pts.add(new Point2D.Double(116.385082,40.104757));//平西王府总站
+   /* boolean result  = IsPtInPoly(point, pts);
+    if(result){
+        System.out.println("点在多边形内");
+    }else{
+        System.out.println("点在多边形外");
+    }*/
+}
+public static boolean IsPtInPoly(Point2D.Double point, List<Point2D.Double> pts){
+    
+    int N = pts.size();
+    boolean boundOrVertex = true; //如果点位于多边形的顶点或边上，也算做点在多边形内，直接返回true
+    int intersectCount = 0;//cross points count of x 
+    double precision = 2e-10; //浮点类型计算时候与0比较时候的容差
+    Point2D.Double p1, p2;//neighbour bound vertices
+    Point2D.Double p = point; //当前点
+    
+    p1 = pts.get(0);//left vertex        
+    for(int i = 1; i <= N; ++i){//check all rays            
+        if(p.equals(p1)){
+            return boundOrVertex;//p is an vertex
+        }
+        
+        p2 = pts.get(i % N);//right vertex            
+        if(p.x < Math.min(p1.x, p2.x) || p.x > Math.max(p1.x, p2.x)){//ray is outside of our interests                
+            p1 = p2; 
+            continue;//next ray left point
+        }
+        
+        if(p.x > Math.min(p1.x, p2.x) && p.x < Math.max(p1.x, p2.x)){//ray is crossing over by the algorithm (common part of)
+            if(p.y <= Math.max(p1.y, p2.y)){//x is before of ray                    
+                if(p1.x == p2.x && p.y >= Math.min(p1.y, p2.y)){//overlies on a horizontal ray
+                    return boundOrVertex;
+                }
+                
+                if(p1.y == p2.y){//ray is vertical                        
+                    if(p1.y == p.y){//overlies on a vertical ray
+                        return boundOrVertex;
+                    }else{//before ray
+                        ++intersectCount;
+                    } 
+                }else{//cross point on the left side                        
+                    double xinters = (p.x - p1.x) * (p2.y - p1.y) / (p2.x - p1.x) + p1.y;//cross point of y                        
+                    if(Math.abs(p.y - xinters) < precision){//overlies on a ray
+                        return boundOrVertex;
+                    }
+                    
+                    if(p.y < xinters){//before ray
+                        ++intersectCount;
+                    } 
+                }
+            }
+        }else{//special case when ray is crossing through the vertex                
+            if(p.x == p2.x && p.y <= p2.y){//p crossing over p2                    
+                Point2D.Double p3 = pts.get((i+1) % N); //next vertex                    
+                if(p.x >= Math.min(p1.x, p3.x) && p.x <= Math.max(p1.x, p3.x)){//p.x lies between p1.x & p3.x
+                    ++intersectCount;
+                }else{
+                    intersectCount += 2;
+                }
+            }
+        }            
+        p1 = p2;//next ray left point
+    }
+    
+    if(intersectCount % 2 == 0){//偶数在多边形外
+        return false;
+    } else { //奇数在多边形内
+        return true;
+    }
+    
+}
 }
